af017a132e7cc6c38d02ab5a66f7a9ca9d329759
Michell Ocaña <ocana.1997@hotmail.com>
2022-07-26 12:56:37 -0300

    feat: length challenge

--- questions/00018-easy-tuple-length/test-cases.ts
+++ questions/00018-easy-tuple-length/test-cases.ts
@@ -1,5 +1,7 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type Length<T extends readonly unknown[]> = T['length']
+
 const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
 const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
 
