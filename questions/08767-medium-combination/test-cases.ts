0f222cf315743fb579efe58d1e113b48ad122add
Michell Ocaña <ocana.1997@hotmail.com>
2022-09-14 23:06:57 -0300

    feat: make combination challenge recursive

--- questions/08767-medium-combination/test-cases.ts
+++ questions/08767-medium-combination/test-cases.ts
@@ -1,31 +1,9 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
-type DoubleCombination<T extends string[]> =
-  T[number] extends infer A | infer B
-  ? A extends string
-  ? B extends string
-  ? Equal<A, B> extends true
-  ? never
-  : `${A} ${B}`
+type Combination<T extends string[], All = T[number], Curr = All> =
+  Curr extends string
+  ? Curr | `${Curr} ${Combination<[], Exclude<All, Curr>>}`
   : never
-  : never
-  : never
-
-type TripleCombination<T extends string[]> =
-  T extends [infer A extends string, infer B extends string, infer C extends string]
-  ? `${A} ${B} ${C}` |
-  `${A} ${C} ${B}` |
-  `${B} ${A} ${C}` |
-  `${B} ${C} ${A}` |
-  `${C} ${A} ${B}` |
-  `${C} ${B} ${A}`
-  : never
-
-type Combination<T extends string[]> = [
-  T[number],
-  DoubleCombination<T>,
-  TripleCombination<T>
-][number]
 
 type cases = [
   Expect<Equal<Combination<['foo', 'bar', 'baz']>,
