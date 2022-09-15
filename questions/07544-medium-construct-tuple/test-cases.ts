c3641a396843f8fc31f921b4ea95ffb80ab2cef3
Michell Ocaña <ocana.1997@hotmail.com>
2022-09-14 23:15:22 -0300

    feat: construct tuple challenge

--- questions/07544-medium-construct-tuple/test-cases.ts
+++ questions/07544-medium-construct-tuple/test-cases.ts
@@ -1,5 +1,8 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type ConstructTuple<Length extends number, Acc extends unknown[] = []> =
+  Acc['length'] extends Length ? Acc : ConstructTuple<Length, [...Acc, unknown]>
+
 type cases = [
   Expect<Equal<ConstructTuple<0>, []>>,
   Expect<Equal<ConstructTuple<2>, [unknown, unknown]>>,
