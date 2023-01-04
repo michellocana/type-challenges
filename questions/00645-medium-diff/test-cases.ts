98e8660ab5f6496ccbd2732d5858dc84aa05fef2
Michell Ocaña <ocana.1997@hotmail.com>
2023-01-04 00:37:52 -0300

    feat: diff challenge

--- questions/00645-medium-diff/test-cases.ts
+++ questions/00645-medium-diff/test-cases.ts
@@ -1,5 +1,15 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type Filter<O extends object, K> = K extends keyof O ? never : K
+
+type MissingKeys<O extends object, O1 extends object> = {
+  [K in keyof O as Filter<O1, K>]: O[K]
+}
+
+type Diff<O extends object, O1 extends object, Acc = MissingKeys<O, O1> & MissingKeys<O1, O>> = {
+  [K in keyof Acc]: Acc[K]
+}
+
 type Foo = {
   name: string
   age: string
