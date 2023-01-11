eee0b936b39ecfcc7904851e14390cd25d5eb4b3
Michell Ocaña <ocana.1997@hotmail.com>
2023-01-11 00:30:57 -0300

    feat: fibonacci challenge

--- questions/04182-medium-fibonacci-sequence/test-cases.ts
+++ questions/04182-medium-fibonacci-sequence/test-cases.ts
@@ -1,5 +1,12 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type Fibonacci<N extends number, F1 extends unknown[] = [], F2 extends unknown[] = [unknown], Acc extends unknown[] = [unknown]> =
+  N extends 1
+    ? 1
+    : Acc['length'] extends N
+      ? F2['length']
+      : Fibonacci<N, F2, [...F1, ...F2], [...Acc, unknown]>
+
 type cases = [
   Expect<Equal<Fibonacci<3>, 2>>,
   Expect<Equal<Fibonacci<8>, 21>>,
